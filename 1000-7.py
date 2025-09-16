6fbeec0102d661f206c2e1a050ec95495c5b3812
notthei <74302768+notthei@users.noreply.github.com>
2025-09-16 11:32:26 +0900

    Update 1000-7.py

--- 1000-7.py
+++ 1000-7.py
@@ -1,5 +1,11 @@
+import time
 num = 1000
 
-while num > 0:
-    print(str(num+7),"- 7 =",num-7)
-    num -= 7
+def subtract_seven_sequence(start):
+    num = start
+    while num > 0:
+        print(f"{num + 7} - 7 = {num - 7}")
+        num -= 7
+        time.sleep(0.3)
+
+subtract_seven_sequence(num)
